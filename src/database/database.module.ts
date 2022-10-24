6744211c3feede8a2c9cc0c020e320718b254644
Leonardo Larrad <leonardolarrad@gmail.com>
2022-10-24 00:54:22 -0400

    Add database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+  })]
+})
+export class DatabaseModule {}
